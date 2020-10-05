e12a0daa0af0c18ffd7894098b107f0cbdccbf2f
SantiagoCardenasR <30352306+SantiagoCardenasR@users.noreply.github.com>
2020-10-05 12:37:57 -0500

    Creación de un modal
    Creación de un modal para mostrar datos y un component de forma dinámica

--- src/components/Covid19.js
+++ src/components/Covid19.js
@@ -1,52 +1,26 @@
-import React, { FC, MouseEvent, useState, useEffect } from "react";
-/* import { Link } from "react-router-dom"; */
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Covid19.css";
-import usePortal from "../ReactPortal";
-import { Global, css } from "@emotion/core";
-/* import body from "../images/cuerpoHumano.png";
-import Button from "@material-ui/core/Button";
-import Tooltip from "@material-ui/core/Tooltip";
-import InfoIcon from "@material-ui/icons/Info"; */
+import body from "../images/cuerpoHumano.png";
+import InfoIcon from "@material-ui/icons/Info";
+import usePortal from "react-cool-portal";
 
 function Covid19() {
-  const [isFadeOut, setIsFadeOut] = useState(false);
-  const { Portal, show, hide, isShow } = usePortal({
-    defaultShow: false,
-    escToHide: false,
+  const { Portal, isShow, toggle } = usePortal({
+    defaultShow: false, // The default visibility of portal, default is true
+    internalShowHide: false, // Disable the built-in show/hide portal functions, default is true
+    onShow: (e) => {
+      // Triggered when portal is shown
+      // The event object will be the parameter of "show(e?)"
+    },
+    onHide: (e) => {
+      // Triggered when portal is hidden
+      // The event object will be the parameter of "hide(e?)", it maybe MouseEvent (on clicks outside) or KeyboardEvent (press ESC key)
+    },
   });
-
-  const close = () => {
-    setIsFadeOut(true);
-  };
-
-  const handleClickBackdrop = (MouseEvent) => {
-    const { id } = MouseEvent.target;
-
-    if (id === "modal" || id === "modal-dialog") close();
-  };
-
-  const handleAnimEnd = () => {
-    if (!isFadeOut) return;
-
-    setIsFadeOut(false);
-    hide();
-  };
-
-  useEffect(() => {
-    const handleKeyDown = (KeyboardEvent) => {
-      if (isShow && KeyboardEvent.keyCode === 27) close();
-    };
-
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [isShow]);
-
   return (
     <div className="covid19">
-      {/*  <section id="icon-boxes" class="icon-boxes covid19__menu">
+      <section id="icon-boxes" class="icon-boxes covid19__menu">
         <div class="container covid19__container">
           <div
             class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0 covid19__cardContainer"
@@ -58,11 +32,32 @@ function Covid19() {
                 <img src={body} alt="bodyCovid"></img>
               </div>
               <div class="btn covid19__botton">
-                <Tooltip title={longText} className="covid19__tooltip">
-                  <Button className={classes.button}>
-                    <InfoIcon className="covid19__infoIcon" />
-                  </Button>
-                </Tooltip>
+                <button onClick={toggle}>
+                  {isShow ? "Close" : "Open"} <InfoIcon />
+                </button>
+                <Portal>
+                  <div
+                    // Now you can use the "isShow" state to handle the CSS animations
+                    className={`modal${isShow ? " modal-open" : ""}`}
+                    tabIndex={-1}
+                  >
+                    <div
+                      className="modal-dialog"
+                      role="dialog"
+                      aria-labelledby="modal-label"
+                      aria-modal="true"
+                    >
+                      <div className="modal-header">
+                        <h5 id="modal-label" className="modal-title">
+                          Modal title
+                        </h5>
+                      </div>
+                      <div className="modal-body">
+                        <p>Modal body text goes here.</p>
+                      </div>
+                    </div>
+                  </div>
+                </Portal>
               </div>
 
               <Link className="covid19__link" to="/salud">
@@ -73,65 +68,7 @@ function Covid19() {
             </div>
           </div>
         </div>
-      </section> */}
-
-      <>
-        <Global
-          styles={css`
-            ${normalize}
-            ${root}
-          `}
-        />
-        <div css={container}>
-          <h1 css={title}>REACT COOL PORTAL</h1>
-          <p css={subtitle}>
-            {
-              "React hook for Portals, which renders modals, dropdowns, tooltips etc. to <body> or else."
-            }
-          </p>
-          <button css={btn} onClick={show} type="button">
-            OPEN MODAL
-          </button>
-          <Portal>
-            <div
-              id="modal"
-              css={[modal, isFadeOut && modalFadeOut]}
-              onClick={handleClickBackdrop}
-              onAnimationEnd={handleAnimEnd}
-              tabIndex={-1}
-              role="dialog"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div id="modal-dialog" css={modalDialog} role="document">
-                <div css={modalContent}>
-                  <div css={modalHeader}>
-                    <h5 css={modalTitle} id="exampleModalLabel">
-                      <span role="img" aria-label="Hello">
-                        👋🏻
-                      </span>{" "}
-                      Hola
-                    </h5>
-                    <button
-                      css={modalClose}
-                      onClick={close}
-                      type="button"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div css={modalBody}>
-                    <p>
-                      You can also close me by pressing the &quot;ESC&quot; key.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Portal>
-        </div>
-      </>
+      </section>
     </div>
   );
 }
