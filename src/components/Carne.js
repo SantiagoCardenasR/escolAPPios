1fdc6ba050f4fd96c74f006589ec86d5580e757c
SantiagoCardenasR <30352306+SantiagoCardenasR@users.noreply.github.com>
2020-10-12 23:13:51 -0500

    changes 2020/10/12
    Cambios códgo QR y página de Carné

--- src/components/Carne.js
+++ src/components/Carne.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import QRCode from 'qrcode.react';
+import {useStateValue} from '../StateProvider'
 import "../styles/Carne.css"
+import userImage from "../images/user.png"
 
 function Carne() {
+    const [{userInfo}, dispatch] = useStateValue();
     return (
         <div className="carne">
-            <p className="carne_prueba">Volcán &#x1f30b;</p>
+            <img src={userImage} className="carne__image"></img>
+            <div className="carne__userInfoContainer">
+                <p>Nombre: {userInfo.user_name}</p>
+                <p>Apellido: {userInfo.user_lastName}</p>
+                <p>email: {userInfo.user_email}</p>
+            </div>
+            <div className="carne__qrCodeContainer">
+                <QRCode value={userInfo.user_id} size="300" bgColor="black" fgColor="#bfe349"/>
+            </div>
         </div>
     )
 }
